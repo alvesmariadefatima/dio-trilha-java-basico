7167c9f37919997f107ee5795102adbc1580d9a1
Maria de Fátima <mnunesalves334@gmail.com>
2023-01-04 15:34:07 -0300

    Sistema SmartTV feito em Java

---
+++ sistema-smart-tv/src/SmartTv.java
@@ -0,0 +1,34 @@
+public class SmartTv {
+    boolean ligada = false;
+    int canal = 1;
+    int volume = 25;
+    public void mudarCanal(int novoCanal){
+        canal = novoCanal;
+    }
+    public void aumentarCanal(){
+        canal++;
+    }
+
+    public void diminuirCanal(){
+        canal--;
+    }
+
+    public void aumentarVolume(){
+        System.out.println("Aumentando o volume para: " + volume);
+        //volume = volume + 1;
+        volume++;
+    }
+
+    public void diminuirVolume(){
+        System.out.println("Diminuindo o volume para: " + volume);
+        //volume = volume - 1;
+        volume--;
+    }
+    public void ligar(){
+        ligada = true;
+    }
+
+    public void desligar(){
+        ligada = false;
+    }
+}
