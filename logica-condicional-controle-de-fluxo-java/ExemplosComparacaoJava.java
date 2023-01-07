5a6bbb501517e45389f90c31a1c947b09cd6d063
Maria de Fátima Nunes Alves <94319702+alvesmariadefatima@users.noreply.github.com>
2023-01-07 15:42:29 -0300

    Exemplos de Lógica Condicional

---
+++ logica-condicional-controle-de-fluxo-java/ExemplosComparacaoJava.java
@@ -0,0 +1,21 @@
+public class ExemplosComparacaoJava{
+    public static void main(String[] args) {
+        int i1 = 10;
+        int i2 = 20;
+        float f1 = 4.5f;
+        float f2 = 2.5f;
+        double d1 = 6.5;
+        double d2 = 9.5;
+        String s1 = "Mary";
+        String s2 = "Mary";
+        String s3 = "Fatinha";
+        boolean b1 = true;
+        boolean b2 = false;
+
+        //exemplos de comparação usando operadores relacionais 
+        System.out.println(f1 == f2);
+        System.out.println(i1 != i2);
+        System.out.println(s1 + s2 + s3);
+        System.out.println(b1 && b2);
+    }
+}
---
+++ logica-condicional-controle-de-fluxo-java/OperadoresLogicos.java
@@ -0,0 +1,14 @@
+public class OperadoresLogicos {
+    public static void main(String[] args) {
+        boolean b1 = true;
+        boolean b2 = false;
+        boolean b3 = true;
+        boolean b4 = false;
+
+        //exemplos de operadores lógicos AND,OR, XOR e NOT
+        System.out.println(b1 && b2);
+        System.out.println(b3 || b2);
+        System.out.println(b4 ^ b2);
+        System.out.println(!b3 && b1);
+    }
+}
